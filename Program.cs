4f3e9b5997ceff0eece63f91e1422be4c8f88418
Alexander Glushchenko <alexander.glushchenko@aspose.com>
2023-05-29 17:21:06 +1000

    Init commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/getdata", () =>
+{
+    return Results.Ok();
+});
+
+app.Run();
